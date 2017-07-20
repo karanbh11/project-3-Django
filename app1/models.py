a8809d64454de3e1f8bf93537aaca73fbf99163a
karan bhardwaj <karan.bhardwaj1108@gmail.com>
2017-07-20 15:56:45 +0530

    Created the model class for storing user data

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+	firstname = models.CharField(max_length=30)
+	lastname = models.CharField(max_length=30)
+	username = models.CharField(max_length=20)
+	email = models.EmailField()
+	password = models.CharField(max_length=30)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
