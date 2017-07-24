15de5251aa75e94df85e882db93fbc932db8db47
karan bhardwaj <karan.bhardwaj1108@gmail.com>
2017-07-24 20:27:51 +0530

    Updated forms.py

--- Project3/forms.py
+++ Project3/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from app1.models import user
+from app1.models import *
 
 class signup(forms.ModelForm):
 	class meta:
@@ -10,3 +10,18 @@ class log_in(forms.ModelForm):
 	class meta:
 		model = user
 		fields = ['username', 'password']
+		
+class post(forms.ModelForm):
+	class Meta:
+		model = post
+		fields = ['image', 'caption']
+
+class like(forms.ModelForm):
+	class Meta:
+		model = LikeModel
+		fields = ['post']
+		
+class comment(forms.ModelForm):
+	class Meta:
+		model = CommentModel
+		fields = ['comment_text', 'post']
